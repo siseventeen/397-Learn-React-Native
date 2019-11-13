c9b7b07a199805ffd0a5adb7bff49369f12ae24c
siseventeen <176776401@qq.com>
2019-11-13 14:53:12 -0600

    done with tutorial code

--- my-new-project/src/styles.js
+++ my-new-project/src/styles.js
@@ -42,4 +42,16 @@ export default StyleSheet.create({
         backgroundColor: "red",
         borderColor: "transparent",
     },
+    galleryContainer: { 
+        bottom: 100 
+    },
+    galleryImageContainer: { 
+        width: 75, 
+        height: 75, 
+        marginRight: 5 
+    },
+    galleryImage: { 
+        width: 75, 
+        height: 75 
+    }
 });
